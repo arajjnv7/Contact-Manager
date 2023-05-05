d73a4f7ff6f48d4eb31fbfd4a2aa1e73f6081a4d
Aryan Raj <arajjnv@gmail.com>
2023-05-05 20:08:16 +0530

    commit

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -8,8 +8,8 @@ const CardContact = (props) => {
                     <div className="header">{name}</div>
                     <div>{email}</div>
                 </div>
-                <i className="trash alternate outline icon" style={{color:"red", marginTop:"7px"}}></i>
-            </div>
+                <i className="trash alternate outline icon" style={{color:"red", marginTop:"7px"}} onClick={() => props.clickHandler(id)}></i>
+        </div>
     )
 }
 
